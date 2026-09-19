c9520cec26b4bac41480cb8f65c8ad68ce979983
klflfodj <3121763659@qq.com>
2026-09-19 22:42:14 +0800

    完善红外发送驱动层

--- lib/ir_send/src/ir_send.cpp
+++ lib/ir_send/src/ir_send.cpp
@@ -1,8 +1,29 @@
 #include "ir_send.h"
 
+#include <IRremote.h>
 
+bool ir_init(ir_send_t* send, const ir_send_config_t* cfg)
+{
+    if (send == NULL || cfg == NULL)
+    {
+        return false;
+    }
 
+    send->cfg = *cfg;
 
+    IrSender.begin(send->cfg.ir_pin);
+    
+    return true;
+}
+
+void ir_send_temp(const uint32_t temp)
+{
+    if (temp == 0) 
+    {
+        return;
+    }
+    IrSender.sendNECMSB(temp, 24);
+}
 
 
 
--- lib/ir_send/src/ir_send.h
+++ lib/ir_send/src/ir_send.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <stdint.h>
+#include <stdbool.h>
 
+typedef struct 
+{
+    uint8_t ir_pin;  //红外发送引脚号
+} ir_send_config_t;
 
+typedef struct 
+{
+    ir_send_config_t cfg;   //红外发送配置参数
+} ir_send_t;
+
+bool ir_init(ir_send_t* send , const ir_send_config_t* cfg);
+void ir_send_temp(const uint32_t temp);
 
 
 
